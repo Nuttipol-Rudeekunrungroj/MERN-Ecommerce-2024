040bfdf254d2da3bb2df0edbcf6971028010f707
nuttipol <nuttipol.r@gmail.com>
2024-11-12 10:44:00 +0700

    Sign up and sign in page implementation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,23 +13,41 @@ import ShoppingHome from "./pages/shopping-view/home";
 import ShoppingAccount from "./pages/shopping-view/account";
 import ShoppingCheckout from "./pages/shopping-view/checkout";
 import ShoppingListing from "./pages/shopping-view/listing";
-
+import CheckAuth from "./components/common/check-auth";
+import UnauthPage from "./pages/unauth-page";
 
 function App() {
+  const isAuthenticated = true;
+  const user = {
+    name : 'nut',
+role : 'admin'
+  };
   return (
     <div className="flex flex-col overflow-hidden bg-white">
       <Routes>
-        <Route path="/auth" element={<AuthLayout />}>
+        <Route path="/auth" element={
+          <CheckAuth isAuthenticated={isAuthenticated} user = {user}>
+          <AuthLayout />
+          </CheckAuth>
+          }>
           <Route path="login" element={<AuthLogin />} />
           <Route path="register" element={<AuthRegister />} />
         </Route>
-        <Route path="/admin" element={<AdminLayout />}>
+        <Route path="/admin" element={
+          <CheckAuth isAuthenticated={isAuthenticated} user = {user}>
+          <AdminLayout />
+          </CheckAuth>
+          }>
           <Route path="dashboard" element={<AdminDashboard />} />
           <Route path="products" element={<AdminProducts />} />
           <Route path="orders" element={<AdminOrders />} />
           <Route path="features" element={<AdminFeatures />} />
         </Route>
-        <Route path="/shop" element={<ShoppingLayout />}>
+        <Route path="/shop" element={
+          <CheckAuth isAuthenticated={isAuthenticated} user = {user}>
+          <ShoppingLayout />
+          </CheckAuth>
+          }>
         <Route path="home" element={<ShoppingHome />} />
           <Route path="listing" element={<ShoppingListing />} />
           <Route path="checkout" element={<ShoppingCheckout/>} />
@@ -40,6 +58,7 @@ function App() {
           
         
         <Route path="*" element={<NotFound/>}/>
+        <Route path="/unauth-page" element={<UnauthPage/>}/>
       </Routes>
     </div>
   );
